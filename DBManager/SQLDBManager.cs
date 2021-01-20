ec169830dc83ef574cd3c39aae09e66c4e07859e
Mike <oba-mlittle@gmail.com>
2021-01-20 10:24:05 -0700

    create new class DBManagerBase and new classes SQLDBManager and PostgresDBManager

---
+++ DBManager/SQLDBManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBManager
+{
+    public class SQLDBManager : DBManagerBase, IDisposable
+    {
+
+        public new void Dispose()
+        {
+            // put SQL specific cleanup
+
+
+            // call base class cleanup
+            base.Dispose();
+
+        }
+    }
+}
